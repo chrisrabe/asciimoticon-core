f3829264ccd0318b950550514708d34f8b5fd874
Chris Rabe <chris.rabe@ikegps.com>
2018-03-01 12:23:41 +1300

    removed themes tests - it should be in task/themed-faces-module branch

--- tests/asciimoticon.modules.tests.js
+++ tests/asciimoticon.modules.tests.js
@@ -1,7 +1,6 @@
 /*jshint esversion:6*/
 
 const expect = require('chai').expect;
-const themesModule = require('../app').themes;
 const facesModule = require('../app').faces;
 
 describe('faces module tests', () => {
@@ -13,15 +12,6 @@ describe('faces module tests', () => {
             expect(face).to.not.equal(null);
             expect(ascii).to.not.equal(null);
             expect(words).to.not.equal(null);
-        }
-    });
-});
-
-describe('themes module tests', () => {
-    it('should allow users to load themed faces', () => {
-        for (const theme in themesModule) {
-            const faces = themesModule[theme];
-            expect(faces).to.not.equal(null);
         }
     });
 });
