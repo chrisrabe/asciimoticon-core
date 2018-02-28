734a97d43eb72ee5c0a0964abf6f5916899a3385
Chris Rabe <chris.rabe@ikegps.com>
2018-03-01 09:32:41 +1300

    added cheek components - also started aggregating parts

---
+++ modules/parts.module.js
@@ -0,0 +1,6 @@
+/*jshint esversion:6*/
+
+module.exports.cheeks = require('../components/parts/cheeks.component');
+module.exports.eyes = require('../components/parts/eye.component');
+module.exports.mouth = require('../components/parts/mouth.component');
+module.exports.face = require('../components/parts/face.component');
