118cd4914ca282eb7f6e2c3c1dd50f3bb86a6dc4
chrisrabe <chris.rabe14@gmail.com>
2019-01-26 12:23:10 +1000

    added the finder in the helper module

--- modules/helper.module.js
+++ modules/helper.module.js
@@ -1,3 +1,5 @@
 const text = require('../components/helper/text.component');
+const finder = require('../components/helper/finder.component');
 
 exports.transpose = text.transpose;
+exports.find = finder.find;
